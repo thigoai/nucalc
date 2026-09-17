9719e89e96dad4af04e8292510d7555d5bf5a96c
thigoai <tfreire352@gmail.com>
2026-09-17 15:18:36 -0300

    feat: Método do ponto fixo

---
+++ src/pontofixo.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float func(float x) {
+	return 2 * (x * x * x * x) + 4 * (x * x * x) + 3 * (x * x) - 10 * x - 15;
+}
+
+float fi_func(float x) {
+    return (2 * (x * x * x * x) + 4 * (x * x * x) + 3 * (x * x) - 15) / 10;
+}
+
+
+float pontofixo(float a, float b, float e, float (*f)(float)) {
+    float x = fi_func((b - a) / 2);
+
+    while (fabs(f(x)) > e) {
+        if(f(a) * f(x) < 0) {
+            b = x;
+        } else {
+            a = x;
+        }
+
+        x = fi_func(x);
+    }
+
+    return x;
+}
+
+
+int main() {
+    float zero = pontofixo(1, 3, 0.0001, func);
+
+    printf("ZERO: %f, F(ZERO): %f\n", zero, func(zero));
+
+    return 0;
+}
