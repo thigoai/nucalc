b9b85964e43b56e183d54545c84ef526a74a555c
RaoniSilvestre <raoni.silvestre@hotmail.com>
2026-09-22 14:41:21 -0300

    fix: fix pontofixo type definition

--- include/pontofixo.h
+++ include/pontofixo.h
@@ -1,6 +1,7 @@
 #ifndef PONTOFIXO_H
 #define PONTOFIXO_H
 
-double pontofixo(double a, double b, double e, double (*f)(double));
+double pontofixo(double a, double b, double e, double (*f)(double),
+                 double (*fi)(double));
 
 #endif
